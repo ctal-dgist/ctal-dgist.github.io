87fab370f48cdd6f4ae19e2dfaee0a54552b57e8
u_shin20 <yooshin0303@dgist.ac.kr>
2024-02-15 23:14:35 +0900

    update css

--- js/seminar_read.js
+++ js/seminar_read.js
@@ -1,20 +1,18 @@
 $(function () {
-    $.getJSON("./data/seminar.json", function (data) {
+    $.getJSON('./data/seminar.json', function (data) {
         var years = Object.keys(data).sort().reverse(); // Get sorted list of years
-        var html = "";
+        var html = '';
 
         $.each(years, function (index, year) {
-            html += '<p class="year">' + year + "</p>\n";
-            html += '<ol class="seminar_list">\n';
+            html += '<p class="year">' + year + '</p>\n';
+            html += '<ol reversed class="seminar_list">\n';
 
             if (data[year] && data[year].length > 0) {
-                data[year] = data[year].sort(
-                    (a, b) => new Date(b.date) - new Date(a.date)
-                );
+                data[year] = data[year].sort((a, b) => new Date(b.date) - new Date(a.date));
 
                 $.each(data[year], function (i, f) {
                     if (f.name.length < 3) {
-                        f.name = f.name + "&nbsp;&nbsp;&nbsp;&nbsp;";
+                        f.name = f.name + '&nbsp;&nbsp;&nbsp;&nbsp;';
                     }
                     const today = new Date();
                     let day = today.getDate();
@@ -23,35 +21,33 @@ $(function () {
                     let currentDate = `${year}.${month}.${day}`;
 
                     const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-                    const diffInDays = Math.round(
-                        (new Date(f.date) - new Date(currentDate)) / oneDay
-                    );
+                    const diffInDays = Math.round((new Date(f.date) - new Date(currentDate)) / oneDay);
                     const isthisweek = diffInDays >= 0 && diffInDays < 7;
                     if (isthisweek) {
                         html +=
                             "<li style='font-weight: bold'>" +
                             "<span class='speaker'> " +
                             f.name +
-                            "</span>&nbsp;&nbsp;&nbsp;&nbsp;" +
+                            '</span>&nbsp;&nbsp;&nbsp;&nbsp;' +
                             f.paper +
-                            "</li>\n";
+                            '</li>\n';
                     } else {
                         html +=
-                            "<li>" +
+                            '<li>' +
                             "<span class='speaker'> " +
                             f.name +
-                            "</span>&nbsp;&nbsp;&nbsp;&nbsp;" +
+                            '</span>&nbsp;&nbsp;&nbsp;&nbsp;' +
                             f.paper +
-                            "</li>\n";
+                            '</li>\n';
                     }
                 });
             } else {
-                html += "<p>No seminars found for " + year + ".</p>\n";
+                html += '<p>No seminars found for ' + year + '.</p>\n';
             }
 
-            html += "</ol>\n";
+            html += '</ol>\n';
         });
 
-        $("#seminarList").html(html);
+        $('#seminarList').html(html);
     });
 });
