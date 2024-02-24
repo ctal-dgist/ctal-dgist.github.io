cead13ea396c488f6d75a299af28e8ee743379ce
u_shin20 <yooshin0303@dgist.ac.kr>
2024-02-24 12:50:40 +0900

    update people

---
+++ js/people.js
@@ -0,0 +1,36 @@
+$(function () {
+    $.getJSON('./data/people.json', function (data) {
+        var professor = data['Professor'];
+        var Ph = data['Ph.D'];
+        var MS = data['MS'];
+        var Intern = data['Intern'];
+        var Alumni = data['Alumni'];
+
+        function addPeople(people, target) {
+            var container = document.getElementById(target);
+            var ul = document.createElement('ul');
+
+            people.forEach(function (person) {
+                var li = document.createElement('li');
+                li.innerHTML = person.name + ' (' + person.name_eng + ')';
+
+                if (target === 'Alumni') {
+                    li.innerHTML += '<br/>' + person.position;
+                    if (person.current) {
+                        li.innerHTML += ', ' + person.current;
+                    }
+                }
+
+                ul.appendChild(li);
+            });
+
+            container.appendChild(ul);
+        }
+
+        addPeople(professor, 'professor');
+        addPeople(Ph, 'Ph');
+        addPeople(MS, 'MS');
+        addPeople(Intern, 'Intern');
+        addPeople(Alumni, 'Alumni');
+    });
+});
