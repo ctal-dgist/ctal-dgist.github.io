9009287f08f48dd4f0aa05acba028a0466cb07c6
u_shin20 <yooshin0303@dgist.ac.kr>
2024-03-11 15:09:36 +0900

    update people

--- js/people.js
+++ js/people.js
@@ -1,23 +1,33 @@
 $(function () {
-    $.getJSON('./data/people.json', function (data) {
-        var professor = data['Professor'];
-        var Ph = data['Ph.D'];
-        var MS = data['MS'];
-        var Intern = data['Intern'];
-        var Alumni = data['Alumni'];
+    $.getJSON("./data/people.json", function (data) {
+        var professor = data["Professor"];
+        var Ph = data["Ph.D"];
+        var MS = data["MS"];
+        var Intern = data["Intern"];
+        var Alumni = data["Alumni"];
 
         function addPeople(people, target) {
             var container = document.getElementById(target);
-            var ul = document.createElement('ul');
+            var ul = document.createElement("ul");
 
             people.forEach(function (person) {
-                var li = document.createElement('li');
-                li.innerHTML = person.name + ' (' + person.name_eng + ')';
+                var li = document.createElement("li");
+                li.innerHTML = "";
 
-                if (target === 'Alumni') {
-                    li.innerHTML += '<br/>' + person.position;
+                if (target !== "Alumni") {
+                    var photo = person.photo;
+                    if (photo == undefined) {
+                        photo = "person.png";
+                    }
+                    photo = "../assets/people/" + photo;
+                    li.innerHTML += `<img src="${photo}"></img>`;
+                }
+                li.innerHTML += person.name + " (" + person.name_eng + ")";
+
+                if (target === "Alumni") {
+                    li.innerHTML += "<br/>" + person.position;
                     if (person.current) {
-                        li.innerHTML += ', ' + person.current;
+                        li.innerHTML += ", " + person.current;
                     }
                 }
 
@@ -27,10 +37,10 @@ $(function () {
             container.appendChild(ul);
         }
 
-        addPeople(professor, 'professor');
-        addPeople(Ph, 'Ph');
-        addPeople(MS, 'MS');
-        addPeople(Intern, 'Intern');
-        addPeople(Alumni, 'Alumni');
+        addPeople(professor, "professor");
+        addPeople(Ph, "Ph");
+        addPeople(MS, "MS");
+        addPeople(Intern, "Intern");
+        addPeople(Alumni, "Alumni");
     });
 });
