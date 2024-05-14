7b1255f8ffa50dcc8feaff61e113c570d2467fe9
u_shin20 <yooshin0303@dgist.ac.kr>
2024-05-14 13:04:35 +0900

    update seminar page

--- js/seminar_read.js
+++ js/seminar_read.js
@@ -1,63 +1,54 @@
 $(function () {
   $.getJSON("./data/seminar.json", function (data) {
-    var years = Object.keys(data).sort().reverse(); // Get sorted list of years
     var html = "";
+    var years = Object.keys(data).sort((a, b) => b - a); // 년도를 내림차순으로 정렬
 
     $.each(years, function (index, year) {
-      html += '<p class="year">' + year + "</p>\n";
-      html += '<ol reversed class="seminar_list">\n';
+      $.each(data[year], function (i, seminar) {
+        let seminarDate = new Date(seminar.date);
+        let today = new Date();
+        let isthisweek =
+          seminarDate >= today &&
+          seminarDate < new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+        let className = isthisweek ? "seminar_card thisweek" : "seminar_card";
 
-      if (data[year] && data[year].length > 0) {
-        data[year] = data[year].sort(
-          (a, b) => new Date(b.date) - new Date(a.date)
-        );
+        // 날짜 구성요소 추출
+        let day = seminarDate.getDate(); // 일
+        let monthNames = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+        let month = monthNames[seminarDate.getMonth()]; // 월 이름으로
+        let year = seminarDate.getFullYear(); // 년도
 
-        $.each(data[year], function (i, f) {
-          if (f.name.length < 3) {
-            f.name = f.name + "&nbsp;&nbsp;&nbsp;&nbsp;";
-          }
-          const today = new Date();
-          let day = today.getDate();
-          let month = today.getMonth() + 1;
-          let year = today.getFullYear();
-          let currentDate = `${year}.${month}.${day}`;
+        let titleHtml = seminar.link
+          ? `<a href="${seminar.link}" class="title">${seminar.paper}</a>`
+          : `<p class="title">${seminar.paper}</p>`;
 
-          const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-          const diffInDays = Math.round(
-            (new Date(f.date) - new Date(currentDate)) / oneDay
-          );
-          const isthisweek = diffInDays >= 0 && diffInDays < 7;
+        html += `<div class="${className}">
 
-          let url = f.link ? new URL(f.link) : null;
-          if (isthisweek) {
-            html += "<li class='thisweek'>" + "<p class='title'>" + f.paper;
+            <div class="date">
+              <p class="day">${day}</p>
+              <p>${month}, ${year}</p>
+            </div>
+            <div class="info">
+              ${titleHtml}
+              <p class="speaker"><i class="fa-regular fa-user"></i> ${seminar.name}</p>
+            </div>
 
-            if (url) {
-              html +=
-                "<a class='link' href='" + url + "'> " + "(link)" + "</a>";
-            }
-
-            html +=
-              "</p>" + "<p class='speaker'> " + f.name + "</p>" + "</li>\n";
-          } else {
-            html += "<li>" + "<p class='title'>" + f.paper;
-
-            if (url) {
-              html +=
-                "<a class='link' href='" + url + "'> " + "(link)" + "</a>";
-            }
-
-            html +=
-              "</p>" + "<p class='speaker'> " + f.name + "</p>" + "</li>\n";
-          }
-        });
-      } else {
-        html += "<p>No seminars found for " + year + ".</p>\n";
-      }
-
-      html += "</ol>\n";
+        </div>`;
+      });
     });
-
     $("#seminarList").html(html);
   });
 });
