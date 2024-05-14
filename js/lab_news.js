a346159de0f2f4ef682c825637b3072b16618d51
u_shin20 <yooshin0303@dgist.ac.kr>
2024-05-14 10:33:59 +0900

    update seminar and news

--- js/lab_news.js
+++ js/lab_news.js
@@ -1,32 +1,54 @@
 $(function () {
-    var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  var monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-    $.getJSON('./data/lab_news.json', function (data) {
-        var year = Object.keys(data).sort().reverse()[0];
-        var newsData = data[year];
-        newsData.sort(function (a, b) {
-            return new Date(b.date) - new Date(a.date);
-        });
-        var html = '<ul>';
+  $.getJSON("./data/lab_news.json", function (data) {
+    var years = Object.keys(data).sort().reverse();
+    var html = "";
 
-        $.each(newsData, function (key, value) {
-            var one_news = '<li>';
+    $.each(years, function (index, year) {
+      var newsData = data[year];
+      newsData.sort(function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
 
-            var date = new Date(value.date);
-            var monthIndex = date.getMonth();
-            var monthName = monthNames[monthIndex];
-            var day = date.getDate();
+      html += "<h3>" + year + "</h3><ul>";
 
-            one_news += '<div class="date"><p>' + monthName + '</p>' + '<p>' + day + '</p></div>';
+      $.each(newsData, function (key, value) {
+        var one_news = "<li>";
 
-            var content = value.content;
+        var date = new Date(value.date);
+        var monthIndex = date.getMonth();
+        var monthName = monthNames[monthIndex];
+        var day = date.getDate();
 
-            one_news += '<div class="content">' + content + '</div>';
+        one_news +=
+          '<div class="date"><p>' +
+          monthName +
+          "</p>" +
+          "<p>" +
+          day +
+          "</p></div>";
+        one_news += '<div class="content">' + value.content + "</div>";
 
-            one_news += '</li>';
-            html += one_news;
-        });
-        html += '</ul>';
-        $('#newsList').html(html);
+        one_news += "</li>";
+        html += one_news;
+      });
+      html += "</ul>";
     });
+
+    $("#newsList").html(html);
+  });
 });
